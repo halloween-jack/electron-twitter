e1303ee0834b1b5401b292f5520b579b1fbd2815
shohei <bokukoko.notsimple@gmail.com>
2016-10-08 15:39:18 +0900

    add timeline component

--- src/app/javascripts/renderer/components/main.jsx
+++ src/app/javascripts/renderer/components/main.jsx
@@ -38,3 +38,17 @@ class Tweet extends React.Component {
     );
   }
 }
+
+class Timeline extends React.Component {
+  render() {
+    const tweets = this.props.tweets.map((tweet) => {
+      return <Tweet tweet={tweet} key={tweet.id}/>;
+    });
+
+    return (
+      <ul className='list-group'>
+        {tweets}
+      </ul>
+    );
+  }
+}
